a6e79f0f01fba58f5ef4bed44dc3504732afeed5
unknown <jhonlegarda1.2@gmail.com>
2025-10-08 17:11:07 -0500

    CONSTS

--- src/app/layout/components/nav-bar/nav-bar.ts
+++ src/app/layout/components/nav-bar/nav-bar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, MatIconModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
