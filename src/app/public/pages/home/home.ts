cc58bb289f398e68c36687e2b5b3c281ef776b59
unknown <jhonlegarda1.2@gmail.com>
2025-10-09 19:51:20 -0500

    Added: cards

--- src/app/public/pages/home/home.ts
+++ src/app/public/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { MatButtonModule } from '@angular/material/button';
 import { Component } from '@angular/core';
+import { MENU_CARD_CONSTANTS } from '../../constants/home-card.constans';
+import { HomeCard } from '../../components/home-card/home-card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, HomeCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  menuCards = MENU_CARD_CONSTANTS;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,ts,scss}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        homeCard: 'linear-gradient(to bottom right, #4A336B 0%, #692E93 100%)',
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
