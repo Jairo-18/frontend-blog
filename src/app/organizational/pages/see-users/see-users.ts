975a0d8ff25f31e3afc93ffd19261d10281ccb89
jhonmartinez31 <124942548+jhonmartinez31@users.noreply.github.com>
2025-10-18 20:36:58 -0500

    tabla de usuarios

--- src/app/organizational/pages/see-users/see-users.ts
+++ src/app/organizational/pages/see-users/see-users.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { UserAdminService } from '../../services/userAdmin.service';
+import { Profile } from '../../../profile/interfaces/profile.interface';
 
 @Component({
-  selector: 'app-see-users',
-  imports: [],
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [CommonModule, MatTableModule, MatProgressSpinnerModule],
   templateUrl: './see-users.html',
-  styleUrl: './see-users.scss'
 })
-export class SeeUsers {
+export class SeeUsers implements OnInit {
+  displayedColumns: string[] = ['fullName', 'email', 'country', 'phone', 'created_at'];
+  users: Profile[] = [];
+  loading: boolean = true;
 
+  constructor(private userAdminService: UserAdminService, private ngZone: NgZone) {}
+
+  ngOnInit() {
+    setTimeout(async () => {
+      try {
+        const { data } = await this.userAdminService.getUsers();
+        this.ngZone.run(() => {
+          this.users = data || [];
+          this.loading = false;
+        });
+      } catch (error) {
+        console.error('❌ Error al obtener usuarios:', error);
+        this.ngZone.run(() => (this.loading = false));
+      }
+    }, 5000);
+  }
 }
